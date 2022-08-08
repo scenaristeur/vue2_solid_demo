fbb1fe5d7458702a3af13dcd6c501caeb12dfa20
scenaristeur <scenaristeur@gmail.com>
2022-08-08 10:02:57 +0200

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
+const publicPath = process.env.NODE_ENV === 'production' ? '/vue2_solid_demo/' : '/'
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: publicPath,
   transpileDependencies: true
 })
