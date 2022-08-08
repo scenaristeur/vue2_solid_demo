34033065bb5150e881d12583af12878d7ff9e382
scenaristeur <scenaristeur@gmail.com>
2022-08-08 13:05:26 +0200

    browser or profile

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,12 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <a href="https://github.com/scenaristeur/vue2_solid" target="_blank">vue2_solid</a> |
-    <a href="https://github.com/scenaristeur/vue2_solid_demo" target="_blank">code source demo</a> |  v.1.0.2
+  <router-view/>
+    <!--
+    <hr>
+    tests
     <InputText />
-    <InputTextarea />
-    <SolidLogin clientName="Vue2Solid_Demo" />
-
-    <router-view/>
-
+    <InputTextarea /> -->
   </div>
 </template>
 
@@ -20,12 +18,13 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
 }
 
 nav {
   padding: 30px;
+  text-align: center;
 }
 
 nav a {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+Vue2Solid Demo
+    <b-container>
+      <a href="https://github.com/scenaristeur/vue2_solid" target="_blank">vue2_solid</a> |
+      <a href="https://github.com/scenaristeur/vue2_solid_demo" target="_blank">code source demo</a> |  v.1.0.2
+      <hr>
+      SolidLogin :     <SolidLogin clientName="Vue2Solid_Demo" />
+
+      SolidBrowser : <SolidBrowser />
+    </b-container>
+
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
