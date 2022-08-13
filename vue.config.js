71197eafe3ba4be5a5801b8dff866a80e63a3753
scenaristeur <scenaristeur@gmail.com>
2022-08-13 10:19:54 +0200

    fix attr in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,19 @@
+const path = require('path')
+
 const publicPath = process.env.NODE_ENV === 'production' ? '/vue2_solid_demo/' : '/'
 
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: publicPath,
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    // https://bootstrap-vue.org/docs#aliasing-vue-import
+    config.resolve.alias.set(
+      'vue$',
+      // If using the runtime only build
+      path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js')
+      // Or if using full build of Vue (runtime + compiler)
+      // path.resolve(__dirname, 'node_modules/vue/dist/vue.esm.js')
+    )
+  }
 })
